32501e4da57cc120f01703b7ff44a310eae22a9f
fgjtfhju <mar_byf@hotmail.com>
2025-06-23 21:48:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from core.bot_runner import run_bot
 
 if __name__ == "__main__":
-    run_bot()
+   run_bot()

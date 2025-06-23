5db7774f222dd211818af501c7770b400c74e0b8
fgjtfhju <mar_byf@hotmail.com>
2025-06-23 21:44:02 +0200

    Add files via upload

---
+++ src/bitget_sdk/rest_api.py
@@ -0,0 +1,6 @@
+class BitgetAPI:
+    def __init__(self):
+        print("Initialized BitgetAPI")
+
+    def place_order(self):
+        print("Order placed.")
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from bitget_sdk.rest_api import BitgetAPI
+
+def main():
+    api = BitgetAPI()
+    print("Bot is running with advanced strategy...")
+
+if __name__ == "__main__":
+    main()
